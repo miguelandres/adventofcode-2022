de394132e80e8a326bec7a70965604b7c5199331
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-12-16 18:05:43 -0500

    solution for day 16 simplified. part 2 still not done :(

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -6,8 +6,7 @@ import utils.readAocInput
 private data class MemoizationPosition(
     val time: Int,
     val location: String,
-    val bitwiseSet: Int,
-    val pressureReleased: Int
+    val valvesOpen: Set<String>
 )
 
 fun main() {
@@ -25,73 +24,54 @@ fun main() {
         )
     }
 
-    val bitwiseMap = graph.entries.map { it.value }.filter { it.value > 0 }
-        .mapIndexed { index, graphNode -> Pair(graphNode.name, 1 shl index) }.toMap()
-    val reverseBitwiseMap = bitwiseMap.entries.associate { Pair(it.value, it.key) }
-    val sumBitwise = bitwiseMap.entries.sumOf { it.value }
-
+    val valvesWithPressure = graph.entries.map { it.value }.filter { it.value > 0 }.map { it.name }.toSet()
     val distances = getAllDistancesFloydWarshall(graph.values.toList())
-
-    val solutions = HashMap<Triple<Int, String, Int>, Int>()
-    // solutions[0][mapLocations["AA"]!!][0] = 0
-
-    var nextSteps = listOf(MemoizationPosition(0, "AA", 0, 0))
+    val solutions = HashMap<MemoizationPosition, Int>()
+    var nextSteps = listOf(Pair(MemoizationPosition(0, "AA", emptySet()), 0))
 
     var max = Int.MIN_VALUE
 
     while (nextSteps.isNotEmpty()) {
-        max = nextSteps.maxOf { it.pressureReleased }.coerceAtLeast(max)
+        max = nextSteps.maxOf { it.second }.coerceAtLeast(max)
         nextSteps = nextSteps.flatMap {
-            if ((
-                solutions[Triple(it.time, it.location, it.bitwiseSet)]
-                    ?: Int.MIN_VALUE
-                ) < it.pressureReleased
-            ) {
-                solutions[Triple(it.time, it.location, it.bitwiseSet)] = it.pressureReleased
-                generateNextSteps(it, graph, distances, bitwiseMap, solutions)
+            if ((solutions[it.first] ?: Int.MIN_VALUE) < it.second) {
+                solutions[it.first] = it.second
+                generateNextSteps(it.first, it.second, graph, distances, valvesWithPressure, solutions)
             } else {
                 emptyList()
             }
         }
     }
     println(max)
-
-    val validEndings = solutions.filter { it.key.first == 26 && ((bitwiseMap[it.key.second] ?: 0) and it.key.third != 0) }
-    val resultWithHelp = validEndings.maxOf { end1 ->
-        validEndings
-            .filter { end2 -> (end1.key.third and end2.key.third) == 0 }
-            .maxOfOrNull { end2 -> end1.value + end2.value } ?: 0
-    }
-
-    println(resultWithHelp)
 }
 
 private fun generateNextSteps(
     pos: MemoizationPosition,
+    pressureReleased: Int,
     graph: Map<String, GraphNode<Int>>,
     distances: Map<Pair<String, String>, Int>,
-    bitwiseMap: Map<String, Int>,
-    solutions: HashMap<Triple<Int, String, Int>, Int>
-): List<MemoizationPosition> {
+    valvesWithPressure: Set<String>,
+    solutions: HashMap<MemoizationPosition, Int>
+): List<Pair<MemoizationPosition, Int>> {
     val moves =
-        bitwiseMap.entries.filter { entry -> entry.value and pos.bitwiseSet == 0 }.map { it.key }.map { destination ->
-            pos.copy(time = pos.time + distances[Pair(pos.location, destination)]!!, location = destination)
-        }.plus(pos.copy(time = pos.time + 1))
-    val bitwiseLocation = bitwiseMap[pos.location]
-    return (
-        if (bitwiseLocation != null && pos.bitwiseSet and bitwiseLocation == 0) {
-            moves.plus(
-                pos.copy(
-                    time = pos.time + 1,
-                    bitwiseSet = pos.bitwiseSet or bitwiseLocation,
-                    pressureReleased = pos.pressureReleased + graph[pos.location]!!.value * (29 - pos.time)
-                )
+        valvesWithPressure.filter { it !in pos.valvesOpen }.map { destination ->
+            Pair(
+                pos.copy(time = pos.time + distances[Pair(pos.location, destination)]!!, location = destination),
+                pressureReleased
             )
-        } else {
-            moves
-        }
-        ).filter {
-        it.time <= 30 &&
-            (solutions[Triple(it.time, it.location, it.bitwiseSet)] ?: Int.MIN_VALUE) < it.pressureReleased
+        }.plus(Pair(pos.copy(time = pos.time + 1), pressureReleased))
+
+    val movesAndOpen = if (pos.location in valvesWithPressure && pos.location !in pos.valvesOpen) {
+        moves.plus(
+            Pair(
+                pos.copy(time = pos.time + 1, valvesOpen = pos.valvesOpen.plus(pos.location)),
+                pressureReleased + graph[pos.location]!!.value * (29 - pos.time)
+            )
+        )
+    } else {
+        moves
+    }
+    return movesAndOpen.filter {
+        it.first.time <= 30 && (solutions[it.first] ?: Int.MIN_VALUE) < it.second
     }
 }
